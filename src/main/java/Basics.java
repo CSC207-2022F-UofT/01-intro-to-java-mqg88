891c8ce0744dc79c8342a6784739349300bbb526
mqg88 <meghan.george@mail.utoronto.ca>
2022-09-11 20:37:33 -0400

    completed tasks 1 through 5

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -42,7 +42,7 @@ public class Basics {
 
         /* TODO (Task 1): Write a line of code below that prints the string
          *                Hello World!
-         */
+         */System.out.println("Hello World!");
 
 
 
@@ -61,7 +61,7 @@ public class Basics {
         /*
          * TODO (Task 2): Create a variable named my_variable and assign it the
          *                value 100.
-         */
+         */int my_variable = 100;
 
 
 
@@ -98,7 +98,9 @@ public class Basics {
          *     ...
          * Current count: 1
          * Current count: 0
-         */
+         */for (int i = 10; i >= 0; i = i - 1){
+             System.out.println("Current count: " + i);
+        }
 
 
     }
@@ -141,11 +143,17 @@ public class Basics {
          *                you may use them if you wish.
          */
         StringBuilder ret = new StringBuilder();
+        String[] strings = to_split.split(" ");
+        for (String word : strings){
+            ret.append(word.charAt(0));
+        }
 
-        // Fill in the rest of the body here
+
+
 
         return ret.toString();
     }
+
 
     /**
      * 7. Below is a method that takes in an integer array (similar to a list
@@ -169,7 +177,15 @@ public class Basics {
          *                .length attribute (e.g. arr.length)
          *                You can index into arrays as we do in Python
          *                (e.g. arr[i] gives you the item at index i).
-         */
+         */int length = arr.length;
+        if (length <= 1){
+             return 0;
+        }else {
+             for (int i = 1; i < length; i = i + 2){
+                 current_sum = current_sum + arr[i];
+             }
+        }
+
 
         return current_sum;
     }
